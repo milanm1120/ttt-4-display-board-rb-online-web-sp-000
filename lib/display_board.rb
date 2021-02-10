3fd3940c4f1ccc99fdfa0c8f7d6d4c82acbcf4e2
Milan Mody <milanm1120@gmail.com>
2021-02-10 03:18:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+  
